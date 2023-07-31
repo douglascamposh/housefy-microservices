1fee6c4fcde078fc2171ab8b8d854202957d0d02
douglas <douglas.campos@mojix.com>
2023-07-31 14:46:39 -0400

    initialize project

---
+++ services/housefy/src/main/java/com/dech/housefy/HousefyApplication.java
@@ -0,0 +1,13 @@
+package com.dech.housefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousefyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousefyApplication.class, args);
+    }
+
+}
