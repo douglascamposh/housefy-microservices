4b4a673f66b98a5684bff9b7f261033b8d091c0a
douglas <douglas.campos@mojix.com>
2023-08-10 15:03:17 -0400

    added objectmapper and validation

--- services/housefy/src/main/java/com/dech/housefy/service/IPropertyService.java
+++ services/housefy/src/main/java/com/dech/housefy/service/IPropertyService.java
@@ -1,11 +1,12 @@
 package com.dech.housefy.service;
 
 import com.dech.housefy.domain.Property;
+import com.dech.housefy.dto.PropertyDTO;
 import com.fasterxml.jackson.databind.annotation.JsonAppend;
 
 import java.util.List;
 
 public interface IPropertyService {
-    List<Property> findAll();
-    Property save(Property property);
+    List<PropertyDTO> findAll();
+    PropertyDTO save(PropertyDTO property);
 }
--- services/housefy/src/main/java/com/dech/housefy/service/impl/PropertyServiceImpl.java
+++ services/housefy/src/main/java/com/dech/housefy/service/impl/PropertyServiceImpl.java
@@ -1,27 +1,33 @@
 package com.dech.housefy.service.impl;
 
 import com.dech.housefy.domain.Property;
+import com.dech.housefy.dto.PropertyDTO;
 import com.dech.housefy.repository.IPropertyRepository;
 import com.dech.housefy.service.IPropertyService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class PropertyServiceImpl implements IPropertyService {
 
     private final IPropertyRepository propertyRepository;
+    private final ModelMapper modelMapper;
 
     @Override
-    public List<Property> findAll() {
-        return propertyRepository.findAll();
+    public List<PropertyDTO> findAll() {
+        return propertyRepository.findAll().stream().map(property -> modelMapper.map(property, PropertyDTO.class)).collect(Collectors.toList());
     }
 
     @Override
-    public Property save(Property property) {
+    public PropertyDTO save(PropertyDTO propertyDTO) {
+        Property property = modelMapper.map(propertyDTO, Property.class);
         Property newProperty = propertyRepository.save(property);
-        return newProperty;
+        return modelMapper.map(newProperty, PropertyDTO.class);
     }
 }
